03ba1d45cd8ec4f8d905c11451dfe3e9b43b1282
hapaxlegomena <thesemariofilms@gmail.com>
2022-12-04 12:33:34 +0000

    Shortened variable declaration

--- InfiniteBijectionSets.java
+++ InfiniteBijectionSets.java
@@ -1,24 +1,18 @@
 import java.util.Arrays;
 public class InfiniteBijectionSets {
     public static void main(String args[]) {
-        int a1 = 0;
-        int a2 = 0;
-        int b2 = 1;
-        int a3 = 0;
-        int b3 = 1;
-        int c3 = 2;
         int naturals = -1;
         int runs = 0;
-        int[] oneSet = new int[] {a1};
-        int[] twoSet = new int[] {a2,b2};
-        int[] threeSet = new int[] {a3,b3,c3};
+        int[] oneSet = new int[] {0};
+        int[] twoSet = new int[] {0,1};
+        int[] threeSet = new int[] {0,1,2};
         for(;;) {
             if (runs % 3 == 0) {
                 naturals += 1;
                 System.out.println(naturals + ": " + Arrays.toString(oneSet));
                 oneSet[0] += 1;
             }
-            if (runs % 3 == 1) {
+            else if (runs % 3 == 1) {
                 if (twoSet[0] + 1 != twoSet[1]) {
                     for (int i = 1; i < twoSet[1]; i++) {
                         naturals += 1;
@@ -30,8 +24,7 @@ public class InfiniteBijectionSets {
                 System.out.println(naturals + ": " + Arrays.toString(twoSet));
                 twoSet[1] += 1;
                 twoSet[0] = 0;
-            }
-            if (runs % 3 == 2) {
+            } else {
                 if (threeSet[1] + 1 != threeSet[2]) {
                     for (int i = 1; i < threeSet[2]; i++) {
                         if (threeSet[0] + 1 != threeSet[1]) {
